d3cb6b5d8bb8a1ec6f025f3e3e66827ab60f7615
bufapiqi <26716594@qq.com>
2015-12-08 19:12:44 +0800

    更改

---
+++ LMS_client/src/main/java/dataservice/storagedataservice/GetDepotExcelDataSer.java
@@ -0,0 +1,10 @@
+package dataservice.storagedataservice;
+
+import java.util.ArrayList;
+
+import po.storagePO.DepotExcelPO;
+
+public interface GetDepotExcelDataSer {
+	
+	public ArrayList<DepotExcelPO> getDepotExcel(String city,String date);
+}
