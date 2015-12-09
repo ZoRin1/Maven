d7740725083c30f1dc65b3c3a3839dc02a44ced8
bufapiqi <26716594@qq.com>
2015-12-09 21:20:46 +0800

    12345

--- LMS_client/src/main/java/presentation/financialstaffui/b1financialstaffui.java
+++ LMS_client/src/main/java/presentation/financialstaffui/b1financialstaffui.java
@@ -163,8 +163,8 @@ public class b1financialstaffui extends JFrame{
 		proController = new ProfitController();
 		profitList = new ProfitListBL();
 		vo = proController.returnPro(date);
-		proController.addPro();
 		ArrayList<ProfitVO> tempList = profitList.getProList();
+		proController.addPro();
 		voList.add(vo);
 		for(int i =0 ; i<tempList.size();i++){
 			voList.add(tempList.get(i));
